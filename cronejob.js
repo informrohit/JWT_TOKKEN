5254f159620802cc79e425e5c1746d0ef6473589
Rohit <rohitku@leewayhertz.com>
2021-07-09 13:19:58 +0530

    initial commit

---
+++ cronejob.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cron = require("node-cron");
+const router = express.Router();
+
+router.post("/schedule",(_,res)=>{
+    try {
+        var d = new DataCue();
+        var min = d.getMinutes();
+
+        cron.schedule('${min + 1}*',() =>{
+            console.log('task completed at ${min+1}');
+        },
+        {
+            scheduled: true,
+            timezone:"Asia/Kolkata",
+        }
+        );
+        res.status(201).json({msg:"job Scheduled"});
+        
+    } catch (error) {
+        
+        res.status(500).send("server error")
+    }
+
+});
+
+
+module.exports = router;
